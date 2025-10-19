66727f815e20edddfeff32d6b527e3014d81f01d
Kaynã <mesquitakayna@gmail.com>
2025-10-19 01:43:46 -0300

    Add synthesis table

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,44 @@
 #include <iomanip>
 #include <string>
 
-int main()
-{
+std::string class_str[] = {
+	"",
+	"RESERVED_KEYWORD",
+	"SYMBOL",
+	"CLASS_STEREOTYPE",
+	"CLASS_ID",
+	"RELATION_STEREOTYPE",
+	"RELATION_ID",
+	"INSTANCE_ID",
+	"NATIVE_DT",
+	"CUSTOM_DT",
+	"META_ATTRIBUTE"
+};
+
+int main() {
 	Scanner scanner = {};
 	bool sucess = scanner.scan();
 
 	if (sucess) {
+		int counts[11] = {};
+
 		for (const Token& token : scanner) {
-			std::string token_class;
+			++counts[token.tokenClass()];
 
-			switch (token.tokenClass())
-			{
-				case RESERVED_KEYWORD:    token_class = "RESERVED_KEYWORD"; break;
-				case SYMBOL: 		      token_class = "SYMBOL"; break;
-				case CLASS_STEREOTYPE:    token_class = "CLASS_STEREOTYPE"; break;
-				case CLASS_ID:            token_class = "CLASS_ID"; break;
-				case RELATION_STEREOTYPE: token_class = "RELATION_STEREOTYPE"; break;
-				case RELATION_ID: 		  token_class = "RELATION_ID"; break;
-				case INSTANCE_ID: 		  token_class = "INSTANCE_ID"; break;
-				case NATIVE_DT:           token_class = "NATIVE_DT"; break;
-				case CUSTOM_DT:           token_class = "CUSTOM_DT"; break;
-				case META_ATTRIBUTE:      token_class = "META_ATTRIBUTE"; break;
-			}
-
-			std::cout << std::left << std::setw(19) << token_class << ": \"" 
+			std::cout << std::left << std::setw(19) << class_str[token.tokenClass()] << ": \"" 
 					  << token.Lexeme() << "\" at line " << token.LineNum()
-					  << ", column " << token.ColumnNum() << "." << std::endl;
+					  << ", column " << token.ColumnNum() << "." << std::endl;		
+		}
+
+		std::cout << std::endl;
+
+		std::cout << "Synthesis Table" << std::endl;
+
+		for (int i = 1; i < 11; i++) {
+			if (counts[i] == 0) continue;
+
+			std::cout << std::left << std::setw(19) << class_str[i] << ": " << counts[i]
+					  << (counts[i] > 1 ? " tokens" : " token") << std::endl;
 		}
 	}
 }
